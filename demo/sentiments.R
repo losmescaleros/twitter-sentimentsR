4c3509ac9c6cb2c36a727a6063a30b6fdd8b1e96
Mitchell <mdnevill@ncsu.edu>
2015-04-02 16:11:04 -0700

    Updated documentation

--- demo/sentiments.R
+++ demo/sentiments.R
@@ -13,9 +13,15 @@ promptForCred = function(){
 ## process, including data retrieval, data pre-processing, feature construction
 ## and selection, and finally, modeling and prediction.
 
+## Next up is data retrieval...
+
 pause()
 
 ## Data Retrieval
+## We'll capture 2,000 tweets with the words "love" and "hate" in them. Then we'll 
+## reduce the data set to just the tweet text. 
+
+pause()
 
 ## First, we'll need your Twitter API credentials.
 promptForCred()
@@ -29,12 +35,18 @@ train.raw = captureTweets(c("love", "hate"), demoOauth, "", 2000)
 # We're only concerned with tweet text
 train.raw = parseTweets(train.raw, simplify = T)
 train.raw = train.raw[,"text"]
+
+## Next is pre-processing...
+
 pause()
 
 ## Data Pre-processing
 
 ## Tweets will contain lots of extra stuff: retweet tags (RT), reply tags (@user), emoji, etc.
 ## We'll need to clean them up before processing.
+
+pause()
+
 # Remove Twitter specific syntax
 train = removeTwitterSyntax(train.raw)
 # Remove punctuation, digits, and http links
@@ -44,30 +56,47 @@ corpus = Corpus(VectorSource(train))
 # Convert to lower case, remove stop words, perform stemming, then remove extra white space
 corpus = cleanCorpus(corpus)
 
+## Now we can construct features...
+
 pause()
 
 ## Feature Construction and Selection
+## We'll create a term-document matrix describing the frequencies of words in each tweet. 
+## We can then use this to select frequent and hopefully valuable features.
 
-# Construct term-document matrix
+pause()
+
+# Construct term-document matrix. We'll leave out wods less than 4 charactors
 tdm = TermDocumentMatrix(corpus, control = list(weighting = weightTfIdf, minWordLength = 4, minDocFreq = 1))
-#love_assocs = rownames(findAssocs(tdm, "love", 0.2))
-#hate_assocs = rownames(findAssocs(tdm, "hate", 0.2))
-# Remove sparse terms to reduce feature selection size to a reasonable number
-
+# Use the tdm to create a data frame for easier manipulation
 m = as.matrix(tdm)
 m.df = as.data.frame(t(m))
-#m.df = m.df[, c("love", "hate", love_assocs, hate_assocs)]
+# We only want to use tweets that contain "love" or "hate " not both.
 m.df = m.df[which(m.df$love >= 1) || which(m.df$hate >= 1),]
+# Classify the tweets
 m.df$Class = ifelse(m.df$love, "love", "hate")
+# Factorise for modelling later
 m.df = factorise(m.df)
+# Now we'll construct the training and testing set. We'll just split the data set in half,
+# one half for training and one half for testing
 traintest = list()
 traintest$trainidx = sample(nrow(m.df), size=nrow(m.df)/2)
 traintest$trainingset = m.df[traintest$trainidx, ]
 traintest$testset = m.df[-traintest$trainidx, ]
 
+## Now we can build the model...
+
 pause()
 
 ## Modeling and prediction
+## We'll use a Hoeffding Tree with a split confidence of 1e-1. For features, we'll use 
+## words that occur 20 or more times, excluding of course "love" and "hate". We can then
+## use the training set to train the model and predict the testing set. The accuracy of
+## this is shown. But we can also update the model based on the testing set, using the 
+## old features we had previously selected. The updated accuracy using this approach is
+## also shown. 
+
+pause()
 
 # We'll use terms that occur 20 or more times as our attributes, excluding love and hate
 freq20 = setdiff(findFreqTerms(tdm, 20), c("love", "hate"))
@@ -75,12 +104,14 @@ vars = freq20
 # Construct the formula, then build the model
 formula = as.formula(paste('Class', paste(vars, collapse=' + '), sep=' ~ '))
 trainStream = datastream_dataframe(data = traintest$trainingset[,c(vars, "Class")])
+# Create the Hoeffding Tree, then train it
 hdt = HoeffdingTree(splitConfidence = "1e-1")
 hdtTrained = trainMOA(model = hdt, formula, data = trainStream, reset = T)
 
 # Get the predicted classes for the training set
 scores = predict(hdtTrained, newdata = traintest$testset[, vars], type="response")
 str(scores)
+# Create the confusion matrix and calculate accuracy
 confmx = table(scores, traintest$testset$Class)
 accuracy = (confmx[1,1] + confmx[2,2]) / sum(confmx)
 sprintf("Accuracy for the test set was %f", accuracy)
@@ -90,6 +121,7 @@ testStream = datastream_dataframe(data = traintest$testset[,c(vars, "Class")])
 hdtTrained = trainMOA(model = hdt, formula, data = testStream, reset = F)
 scores = predict(hdtTrained, newdata = traintest$testset[, vars], type="response")
 str(scores)
+# Calculate the updated accuracy
 confmx = table(scores, traintest$testset$Class)
 accuracy = (confmx[1,1] + confmx[2,2]) / sum(confmx)
 # Show the updated accuracy
